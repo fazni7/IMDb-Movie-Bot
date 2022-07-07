cd330e76f4f8d648242a0cd8abb0a618ae5b1d03
Faznj <108731444+fazni7@users.noreply.github.com>
2022-07-07 15:09:38 +0530

    Update script.py

--- script.py
+++ script.py
@@ -24,10 +24,10 @@ IMDb is a Open source project.
 Source: <a href='https://github.com/josprojects/tgmoviebot'>GitHub - Click here 👈</a>
 
 <b>DEVS:</b>
-- <a href='https://t.me/josprojects'>Jos Projects</a>
+- <a href='https://t.me/TamilRockers_FZN'>Tamil Rockers</a>
 
 <b>SUPPORT GROUP</b>
-- <a href='https://t.me/+y53tWFUw6Q43NzE9'>Jos Movie Club</a>"""
+- <a href='https://t.me/+60DRZqE8cEM5MjQ1'>Tamil Rockers Group</a>"""
 
     MANUALFILTER_TXT = """Help: <b>Filters</b>
 
